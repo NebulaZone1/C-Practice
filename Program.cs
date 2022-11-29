1c7f6ee2f5314641b669715f084d1dc860d88020
FizzyBot <mranimationmaker27flip@gmail.com>
2022-11-28 21:04:52 -0500

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace My_CSharp_Programing
+{
+   class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Skynet";
+            Console.ForegroundColor = ConsoleColor.DarkMagenta;
+
+            Console.WriteLine("Hello People!");
+            Console.ReadKey();
+        }
+    }
+}
